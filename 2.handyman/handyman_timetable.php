f526e2e70fc5bb3d8901d659219ea884214d7db0
Lyuei <leeyuwei02@outlook.com>
2022-01-16 22:13:35 +0800

    a

--- 2.handyman/handyman_timetable.php
+++ 2.handyman/handyman_timetable.php
@@ -18,6 +18,7 @@
             include '../shared/sidebar.php';
             
             mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+            $handyman_IC = $_SESSION['handymanlogin'];
             $query = 
             "SELECT a.ID, a.service_type, a.a_time, a.a_date, a.description, s.senior_IC, s.fname, s.lname
             FROM tbl_appointment a LEFT JOIN tbl_senior s 
