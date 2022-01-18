b3b58d4707c450b591114c9ae3e3c3c75aa16413
Lyuei <leeyuwei02@outlook.com>
2022-01-19 03:15:30 +0800

    updated on search worker ttb

--- 3.admin/search_worker_timetable.php
+++ 3.admin/search_worker_timetable.php
@@ -82,7 +82,21 @@
                 <p style="line-height: 1.5em; height: 3em;">' . $value['description'] . '</p>
                 </div>
                 <div class="data">
-                <img src="../img_upload/appointment/' . $value['image'] . '"alt = "Appointment Image">
+                <div class="responsive">
+                <div class="gImg">
+                    <img src="../img_upload/appointment/' . $value['image'] . '"alt = "Appointment Image">
+                </div>
+                </div>
+
+                <div class="clearfix"></div>
+
+                <!-- The Modal -->
+                <div id="myModal" class="modal">
+                <span class="close">×</span>
+                <img class="modal-content" id="img01">
+                <div id="caption"></div>
+                </div>
+                </div>
                 </div>
                 </div>
                 </div>';
@@ -468,8 +482,37 @@
             </table>
         </div>
     </div>
-    </div>
+<script>
+    // Get the modal
+    var modal = document.getElementById('myModal');
 
+    modal.addEventListener('click',function(){
+    this.style.display="none";
+    })
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // Get all images and insert the clicked image inside the modal
+    // Get the content of the image description and insert it inside the modal image caption
+    var images = document.getElementsByTagName('img');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    var i;
+    for (i = 0; i < images.length; i++) {
+    images[i].onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.alt = this.alt;
+        captionText.innerHTML = this.nextElementSibling.innerHTML;
+    }
+    }
+</script>
 </body>
 
 </html>
